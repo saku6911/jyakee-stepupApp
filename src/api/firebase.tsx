6f3dc79688d47a610cef2134e0c0bfa3fc13ef26
saku6911 <sakuykash6911@gmail.com>
2025-05-12 10:16:23 +0900

    fix

--- src/api/firebase.tsx
+++ src/api/firebase.tsx
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getDoc, doc } from "firebase/firestore";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_VITE_STORAGE_BUCKET,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
@@ -15,3 +16,15 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const userDoc = await getDoc(doc(db, "users", user.uid));
+    if (userDoc.exists()) {
+      console.log("User data:", userDoc.data());
+    } else {
+      console.log("Firestoreにユーザー情報がありません");
+    }
+  } else {
+    console.log("ログインしていません");
+  }
+});
